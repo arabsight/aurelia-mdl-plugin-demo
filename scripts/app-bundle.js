69164e7c17f99fe86593bff5da9334b3dd0e16bb
Rabah Ghodbane <r-ghodbane@live.com>
2016-09-18 12:16:01 +0100

    Sun Sep 18 12:16:01 CET 2016

---
+++ scripts/app-bundle.js
@@ -0,0 +1,73 @@
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    exports.configure = configure;
+
+    var _environment2 = _interopRequireDefault(_environment);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    function configure(aurelia) {
+        aurelia.use.standardConfiguration().plugin('aurelia-animator-css').plugin('aurelia-validation').plugin('aurelia-mdl-plugin').feature('resources');
+
+        if (_environment2.default.debug) {
+            aurelia.use.developmentLogging();
+        }
+
+        if (_environment2.default.testing) {
+            aurelia.use.plugin('aurelia-testing');
+        }
+
+        aurelia.start().then(function () {
+            return aurelia.setRoot('shell/default');
+        });
+    }
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('shell/default',['exports'], function (exports) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var Default = exports.Default = function Default() {
+        _classCallCheck(this, Default);
+
+        this.message = 'Hello World!';
+    };
+});
+define('text!shell/default.html', ['module'], function(module) { module.exports = "<template>\n    <h1>${message}</h1>\n</template>\n"; });
+//# sourceMappingURL=app-bundle.js.map
